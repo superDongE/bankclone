4b79292b3d5221e236d61f92796325a07e9ada2c
superDongE <dlagoehd13@gmail.com>
2020-12-01 03:50:03 +0900

    뱅크샐러드 프론트

---
+++ app/src/main/java/com/example/bank/MainActivity.kt
@@ -0,0 +1,14 @@
+package com.example.bank
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.view.View
+import android.widget.ListView
+import androidx.core.content.ContextCompat
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
